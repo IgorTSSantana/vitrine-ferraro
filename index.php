f7c9c110771a03065556607947ee9911bb4e4121
Igor Santana <igortssantana23@gmail.com>
2020-10-14 19:08:36 -0300

    footer

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
-<?php 
-echo "test";
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/bootstrap/css/bootstrap.min.css'?>">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/footer.css'?>">
+<?php
+
+
+get_footer();
