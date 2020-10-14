2286ca3db39552add1b24762be19041504be4986
Felipe Borges <felipercborges@gmail.com>
2020-10-14 19:01:45 -0300

    Header e Carrossel

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+
+    <title>Vitrine Ferraro</title>
+
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/reset.css'?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/bootstrap/css/bootstrap.min.css'?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/header.css'?>">
+    <script src="<?php echo get_template_directory_uri() . '/public/js/jquery.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri() . '/public/bootstrap/js/bootstrap.min.js'?>"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+
+        <div id="area-logo">
+            <h1>Tec<span class="branco">Blog</span></h1>
+        </div>
+
+        <div id="area-menu">
+            <a href="index.html">Home</a>
+            <a href="jogos.html">Jogos</a>
+            <a href="celulares.html">Celulares</a>
+            <a href="informatica.html">Informática</a>
+            <a href="eletronicos.html">Eletrônicos</a>
+        </div>
+
+        <div id="slogan">Veja como você nunca viu!</div>
+
+    </header>
--- index.php
+++ index.php
@@ -1,2 +1,52 @@
-<?php 
-echo "test";
+<?php
+get_header();
+$post_destaques = [
+    'post_type' => 'post',
+    'posts_per_page' => 3,
+    'category_name' => 'carrossel'
+];
+?>
+<section class="container">
+    <?php if (have_posts()):?>
+    <?php $query = new WP_Query($post_destaques); ?>
+    <?php if ($query->have_posts()) : ?>
+    <?php $post = $posts[0]; ?>
+    <?php $contador_carrossel = 0; ?>
+    <?php $contador_indicador = 0; ?>
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <?php while ($contador_indicador < 3) : ?>
+            <li data-target="#carousel-slide" data-slide-to="<?php echo $contador_indicador++; ?>"
+                class="<?php if ($contador_indicador === 1) echo 'active'; ?>"></li>
+            <?php endwhile; ?>
+        </ol>
+        <div class="carousel-inner">
+            <?php while ($query->have_posts()) : ?>
+            <?php $query->the_post(); ?>
+            <div
+                class="carousel-item<?php $contador_carrossel++ ?> <?php if ($contador_carrossel === 1) echo ' active'; ?>">
+                <?php the_post_thumbnail('post_thumbnail', ['id' => 'carousel-img', 'class' => 'carousel-img', 'alt' => 'First Slide']); ?>
+                <div class="carousel-caption">
+                    <a class="link-carrossel" href="<?php echo get_permalink(); ?>">
+                        <div class="noticia_carrosel">
+                            <h3><?php the_title(); ?></h3>
+                            <span class="d-none d-md-block"><?php the_excerpt(); ?></span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <?php wp_reset_query(); ?>
+            <?php endwhile; ?>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        <?php endif; ?>
+    </div>
+    <?php endif; ?>
+</section>
