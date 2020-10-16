7cd0e78b395d1c7951b3525847561a56832bd52c
Felipe Borges <felipercborges@gmail.com>
2020-10-16 20:01:34 -0300

    Add style.css

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css"
         href="<?php echo get_template_directory_uri() . '/public/bootstrap/css/bootstrap.min.css'?>">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/style.css'?>">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/index.css'?>">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/contato.css'?>">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/header.css'?>">
