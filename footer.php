d0314844ff38af2ed9bdc96a14b43757aedd5238
Igor Santana <igortssantana23@gmail.com>
2020-10-16 20:06:06 -0300

    rodapé

--- footer.php
+++ footer.php
@@ -1,11 +1,6 @@
-        <footer>
-            <div class="container">
-                <section style="height:80px;"></section>
-                <div class="row" style="text-align:center;">
-                    <h2>Bootstrap Dark Footer UI</h2>
-                </div>
-                <!----------- Footer ------------>
-                <footer class="footer-bs">
+
+                    <!----------- Footer ------------>
+                    <footer class="footer-bs">
                     <div class="row">
                         <div class="col-md-4 footer-brand animated fadeInLeft">
                             <h2>Logo</h2>
@@ -18,39 +13,27 @@
                             <h4>Menu —</h4>
                             <div class="col-md-6">
                                 <ul class="pages">
-                                    <li><a href="#">Travel</a></li>
-                                    <li><a href="#">Nature</a></li>
-                                    <li><a href="#">Explores</a></li>
-                                    <li><a href="#">Science</a></li>
-                                    <li><a href="#">Advice</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-md-6">
-                                <ul class="list">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Contacts</a></li>
-                                    <li><a href="#">Terms & Condition</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Produtos</a></li>
+                                    <li><a href="#">Sobre a Empresa</a></li>
+                                    <li><a href="#">Contato</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-md-4 footer-social animated fadeInDown">
-                            <h4>Follow Us</h4>
+                            <h4>Rede Sociais</h4>
                             <ul>
-                                <li><a href="#">Facebook</a></li>
-                                <li><a href="#">Twitter</a></li>
-                                <li><a href="#">Instagram</a></li>
-                                <li><a href="#">RSS</a></li>
+                                <li><a href="https://www.facebook.com/profile.php?id=100007223254486" target="_blank">Facebook</a></li>
+                                <li><a href="https://www.instagram.com/ferraro_optica/?hl=pt-br" target="_blank">Instagram</a></li>
                             </ul>
                         </div>
                     </div>
                 </footer>
-                <section style="text-align:center; margin:10px auto;">
-                    <p>Desenvolvido por <a href="https://github.com/IgorTeixas" target="_blank">Igor Santana</a> e <a
+                <section class="rodape-desenvolvedores">
+                    <p class="rodape-paragrafo">Desenvolvido por <a href="https://github.com/IgorTeixas" target="_blank">Igor Santana</a> e <a
                             href="https://github.com/Felipercb" target="_blank">Felipe Borges</a></p>
                 </section>
 
-        </footer>
 
         </body>
 
