9948cf16edf92cd05777e0c789a2245024bfc5e9
Igor Santana <igortssantana23@gmail.com>
2020-10-28 21:30:21 -0300

    header e produtos

--- header.php
+++ header.php
@@ -6,75 +6,62 @@
 
     <meta charset="utf-8">
 
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/reset.css'?>">
-    <link rel="stylesheet" type="text/css"
-        href="<?php echo get_template_directory_uri() . '/public/bootstrap/css/bootstrap.min.css'?>">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/style.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/index.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/contato.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/header.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/footer.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/sobre.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/produtos.css'?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/reset.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/bootstrap/css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/style.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/index.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/contato.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/header.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/footer.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/sobre.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/public/css/produtos.css' ?>">
 
-    <script src="<?php echo get_template_directory_uri() . '/public/js/jquery.min.js'?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="<?php echo get_template_directory_uri() . '/public/js/jquery.min.js' ?>"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="<?php echo get_template_directory_uri() . '/public/bootstrap/js/bootstrap.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri() . '/public/bootstrap/js/bootstrap.min.js' ?>"></script>
 
 
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
 
 </head>
 <?php
-    $cat_args = [
-        // 'orderby'    => 'name',
-        // 'order'      => 'desc',
-        'hide_empty' => false,
-    ];
+$cat_args = [
+    // 'orderby'    => 'name',
+    // 'order'      => 'desc',
+    'hide_empty' => false,
+];
 
-    $product_categories = get_terms('product_cat', $cat_args);
-    ?>
+$product_categories = get_terms('product_cat', $cat_args);
+?>
 
 <body>
     <header>
-
         <div id="area-logo">
-            <img class="imagem-logo" style="width:560px;height:200px;"
-                src="<?php echo get_template_directory_uri() . '/public/img/logo2.png' ?>">
-
+            <img class="imagem-logo" style="width:560px;height:200px;" src="<?php echo get_template_directory_uri() . '/public/img/logo2.png' ?>">
         </div>
         <span class="slogan">Enxergue como você nunca viu!</span>
-
         <div id="area-menu">
-           | <a class="menu-link" href="<?php echo get_site_url() . '/' ?>">HOME</a> |
+            | <a class="menu-link" href="<?php echo get_site_url() . '/' ?>">HOME</a> |
             <?php if (!empty($product_categories)) : ?>
-            <div class="btn-group menu-link">
-                <button id="todos-produtos" type="button" class="menu-link botao-produtos">PRODUTOS</button>
-                <button type="button" class="menu-link dropdown-toggle dropdown-toggle-split" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    <span class="sr-only menu-link">Toggle Dropdown</span>
-                </button>
-                <div class="dropdown-menu menu-link dropdown-link">
-
-                    <?php foreach ($product_categories as $key => $category) : ?>
-                    <?php if($category->slug !='sem-categoria') :?>
-                    <a class="dropdown-item categorias-produtos" href="<?= get_term_link($category) ?>">
-                        <?= $category->name ?>
-                    </a>
-                    <?php endif; ?>
-                    <?php endforeach; ?>
-
-
-                </div>
+                <div class="btn-group menu-link">
+                    <button id="todos-produtos" type="button" class="menu-link botao-produtos">PRODUTOS</button>
+                    <button type="button" class="menu-link dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="sr-only menu-link">Toggle Dropdown</span>
+                    </button>
+                    <div class="dropdown-menu menu-link dropdown-link">
+                        <?php foreach ($product_categories as $key => $category) : ?>
+                            <?php if ($category->slug != 'sem-categoria') : ?>
+                                <a class="dropdown-item categorias-produtos" href="<?= get_site_url() . '/produtos?categoria=' . $category->slug ?>">
+                                    <?= $category->name ?>
+                                </a>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
                 <?php endif; ?>
-            </div>
-
-           | <a class="menu-link" href="<?php echo get_template_directory_uri() . '/sobre' ?>">SOBRE A EMPRESA</a> |
-            <a class="menu-link" href="<?php echo get_template_directory_uri() . '/contato' ?>">CONTATO</a> |
-
+                </div>
+                | <a class="menu-link" href="<?php echo get_template_directory_uri() . '/sobre' ?>">SOBRE A EMPRESA</a> |
+                <a class="menu-link" href="<?php echo get_template_directory_uri() . '/contato' ?>">CONTATO</a> |
         </div>
     </header>
--- page-produtos.php
+++ page-produtos.php
@@ -12,134 +12,106 @@ $filtro_atributo = $_GET['atributo'];
 $args = [
     'post_type' => 'product',
     'posts_per_page' => 10,
-    // 'product_cat' => $filtro_categoria,
+    'product_cat' => $filtro_categoria,
     'paged' => $paged
 ];
-if(isset($filtro_atributo)) {
-     $args['meta_query']=$filtro_atributo;
+if (isset($filtro_atributo)) {
+    $args['meta_query'] = $filtro_atributo;
 }
 ?>
 
 <div class="container container-top">
-
-
     <div class="row titulo-site">
         <h1>
             <span class="titulo-pagina"><?php the_title(); ?></span>
         </h1>
     </div>
-
-
-
-    <section class="row contato-conteudo">
-
-        <div class="col-md-2">
-            <p class="texto-genero">Escolha o gênero de armação desejado:</p>
-            <div>
-                <input type="radio" id="sexo" name="atributo" value="masculino">
-                <label for="sexo">Masculino</label>
+    <?php $loop = new WP_Query($args); ?>
+    <?php if ($loop->have_posts()) : ?>
+        <section class="row contato-conteudo">
+            <div class="col-md-2">
+                <p class="texto-genero">Escolha o gênero de armação desejado:</p>
+                <div>
+                    <input type="radio" id="sexo" name="atributo" value="masculino">
+                    <label for="sexo">Masculino</label>
+                </div>
+                <div>
+                    <input type="radio" id="sexo" name="atributo" value="feminino">
+                    <label for="sexo">Feminino</label>
+                </div>
+                <div>
+                    <input type="radio" id="sexo" name="atributo" value="todos" checked>
+                    <label for="sexo">Todos</label>
+                </div>
+                <button class="btn btn-primary" type="button" id="botao-filtrar">
+                    Aplicar
+                </button>
             </div>
-
-            <div>
-                <input type="radio" id="sexo" name="atributo" value="feminino">
-                <label for="sexo">Feminino</label>
-            </div>
-            <div>
-                <input type="radio" id="sexo" name="atributo" value="todos" checked>
-                <label for="sexo">Todos</label>
-            </div>
-            <button class="btn btn-primary" type="button" id="botao-filtrar">
-                Aplicar
-            </button>
-        </div>
-
-        <div class="col-md-10">
-
-            <?php $loop = new WP_Query($args); ?>
-            <?php if ($loop->have_posts()) : ?>
-            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-
-            <?php $preco=str_replace(".",",", get_post_meta(get_the_ID(),'_regular_price',true)) ?>
-
-            <?php $product = new WC_product(get_the_ID());
-            $attachment_ids = $product->get_gallery_image_ids();
-            ?>
-            <div class="card card-produto">
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <?php the_post_thumbnail('thumbnail', array('class' => 'imagem-produto')); ?>
-
+            <div class="col-md-10">
+                <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                    <?php $preco = str_replace(".", ",", get_post_meta(get_the_ID(), '_regular_price', true)) ?>
+                    <?php $product = new WC_product(get_the_ID()); ?>
+                    <?php $attachment_ids = $product->get_gallery_image_ids(); ?>
+                    <div class="card card-produto">
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <?php the_post_thumbnail('thumbnail', array('class' => 'imagem-produto')); ?>
+                                </div>
+                                <?php foreach ($attachment_ids as $attachment_id) : ?>
+                                    <div class="carousel-item">
+                                        <?php echo wp_get_attachment_image($attachment_id, 'thumbnail', "", ["class" => "imagem-produto"]); ?>
+                                    </div>
+                                <?php endforeach ?>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
-
-                        <?php foreach( $attachment_ids as $attachment_id ) : ?>
-                        <div class="carousel-item">
-                        <?php echo wp_get_attachment_image($attachment_id, 'thumbnail', "", ["class" => "imagem-produto"] ); ?>
-
+                        <div class="card-body">
+                            <span class="codigo-produto">
+                                <?php echo get_post_meta(get_the_ID(), '_sku', true) ?>
+                            </span> <br>
+                            <span class="titulo-produto">
+                                <?= get_the_title() ?> <br>
+                            </span>
+                            <span class="sexo">
+                                <?php echo array_shift(wc_get_product_terms(get_the_ID(), 'pa_sexo', array('fields' => 'names'))); ?>
+                            </span> <br>
+                            <span class="preco">
+                                R$<?= $preco ?>
+                            </span>
                         </div>
-                        <?php endforeach ?>
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-                <div class="card-body">
-                    <span class="codigo-produto">
-
-                        <?php echo get_post_meta(get_the_ID(),'_sku',true) ?>
-
-                    </span> <br>
-                    <span class="titulo-produto">
-                        <?= get_the_title() ?> <br>
-                    </span>
-                    <span class="sexo">
-
-                        <?php echo array_shift( wc_get_product_terms( get_the_ID(), 'pa_sexo', array( 'fields' => 'names' ) ) ); ?>
-
-                    </span> <br>
-                    <span class="preco">
-                        R$<?=$preco?>
-                    </span>
-                </div>
+                <?php endwhile; ?>
+                <?php wp_reset_query(); ?>
+                <nav>
+                    <ul>
+                        <li><?php previous_posts_link('&laquo; PREV', $loop->max_num_pages) ?></li>
+                        <li><?php next_posts_link('NEXT &raquo;', $loop->max_num_pages) ?></li>
+                    </ul>
+                </nav>
             </div>
+        </section>
+        <div class="clear"></div>
+        <?php comments_template(); ?>
 
-
-
-
-
-            <?php endwhile; ?>
-            <?php wp_reset_query(); ?>
-            <nav>
-                <ul>
-                    <li><?php previous_posts_link('&laquo; PREV', $loop->max_num_pages) ?></li>
-                    <li><?php next_posts_link('NEXT &raquo;', $loop->max_num_pages) ?></li>
-                </ul>
-            </nav>
-        </div>
-    </section>
-    <div class="clear"></div>
-    <?php comments_template(); ?>
-
-    <?php comments_popup_link('Sem Comentários', '1 Comentário', '% Comentários', 'comments-link', ''); ?>
+        <?php comments_popup_link('Sem Comentários', '1 Comentário', '% Comentários', 'comments-link', ''); ?>
     <?php else : ?>
-    <div class="artigo">
-        <h2>Nada Encontrado</h2>
-        <p>Erro 404</p>
-        <p>Lamentamos mas não foram encontrados artigos.</p>
-    </div>
+        <div class="artigo">
+            <h2>Nenhum produto cadastrado.</h2>
+        </div>
     <?php endif; ?>
-
 </div>
-
 <script>
-    $('body').on('click','#botao-filtrar',function(){
-        let filtro=$('input[name="atributo"]:checked')
-        window.location.href=window.location.origin + '/wordpress/produtos?atributo=' + filtro.val()
+    $('body').on('click', '#botao-filtrar', function() {
+        let filtro = $('input[name="atributo"]:checked')
+        window.location.href = window.location.origin + '/wordpress/produtos?atributo=' + filtro.val()
     })
 </script>
 
