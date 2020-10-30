0ef603505d9772e822d21e3bb025fcc3906c33c3
Igor Santana <igortssantana23@gmail.com>
2020-10-30 12:23:08 -0300

    page produtos

--- page-produtos.php
+++ page-produtos.php
@@ -13,10 +13,11 @@ $args = [
     'post_type' => 'product',
     'posts_per_page' => 10,
     'product_cat' => $filtro_categoria,
-    'paged' => $paged
+    'paged' => $paged,
 ];
-if (isset($filtro_atributo)) {
-    $args['meta_query'] = $filtro_atributo;
+if (!is_null($filtro_atributo) && $filtro_atributo != 'todos') {
+    $args['taxonomy'] = 'pa_sexo';
+    $args['term'] = $filtro_atributo;
 }
 ?>
 
@@ -32,16 +33,16 @@ if (isset($filtro_atributo)) {
             <div class="col-md-2">
                 <p class="texto-genero">Escolha o gênero de armação desejado:</p>
                 <div>
-                    <input type="radio" id="sexo" name="atributo" value="masculino">
-                    <label for="sexo">Masculino</label>
+                    <input type="radio" id="sexo_masculino" name="atributo" value="masculino">
+                    <label for="sexo_masculino">Masculino</label>
                 </div>
                 <div>
-                    <input type="radio" id="sexo" name="atributo" value="feminino">
-                    <label for="sexo">Feminino</label>
+                    <input type="radio" id="sexo_feminino" name="atributo" value="feminino">
+                    <label for="sexo_feminino">Feminino</label>
                 </div>
                 <div>
-                    <input type="radio" id="sexo" name="atributo" value="todos" checked>
-                    <label for="sexo">Todos</label>
+                    <input type="radio" id="sexo_todos" name="atributo" value="todos" checked>
+                    <label for="sexo_todos">Todos</label>
                 </div>
                 <button class="btn btn-primary" type="button" id="botao-filtrar">
                     Aplicar
@@ -53,7 +54,7 @@ if (isset($filtro_atributo)) {
                     <?php $product = new WC_product(get_the_ID()); ?>
                     <?php $attachment_ids = $product->get_gallery_image_ids(); ?>
                     <div class="card card-produto">
-                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div id="carrossel_<?= $product->id?>" class="carousel slide" data-ride="carousel">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
                                     <?php the_post_thumbnail('thumbnail', array('class' => 'imagem-produto')); ?>
@@ -64,11 +65,11 @@ if (isset($filtro_atributo)) {
                                     </div>
                                 <?php endforeach ?>
                             </div>
-                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <a class="carousel-control-prev" href="#carrossel_<?= $product->id?>" role="button" data-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
                             </a>
-                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <a class="carousel-control-next" href="#carrossel_<?= $product->id?>" role="button" data-slide="next">
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                             </a>
@@ -82,6 +83,10 @@ if (isset($filtro_atributo)) {
                             </span>
                             <span class="sexo">
                                 <?php echo array_shift(wc_get_product_terms(get_the_ID(), 'pa_sexo', array('fields' => 'names'))); ?>
+                            </span> <br>
+                            <span class="categoria">
+                                <?php $term = get_term_by('id', $product->category_ids[0], 'product_cat'); ?>
+                                <?= $term->name ?>
                             </span> <br>
                             <span class="preco">
                                 R$<?= $preco ?>
@@ -109,10 +114,63 @@ if (isset($filtro_atributo)) {
     <?php endif; ?>
 </div>
 <script>
+    $(document).ready(function() {
+        if (window.location.search) {
+            let paramsUrl = [],
+                params = window.location.search.replace('?', '').split('&'),
+                atributoValor = '';
+
+            params.map(function(index, element) {
+                paramsUrl.push(index.split('='));
+            });
+
+            paramsUrl.map(function(element, index) {
+                if (element[0] == 'atributo') {
+                    atributoValor = element;
+                }
+            });
+
+            $('input[name="atributo"]').map(function(index, element) {
+                if (element.value == atributoValor[1]) {
+                    $(element).click();
+                }
+            });
+        }
+    });
+
     $('body').on('click', '#botao-filtrar', function() {
-        let filtro = $('input[name="atributo"]:checked')
-        window.location.href = window.location.origin + '/wordpress/produtos?atributo=' + filtro.val()
-    })
+        let filtro = $('input[name="atributo"]:checked');
+
+        if (window.location.search) {
+            let paramsUrl = [],
+                params = window.location.search.replace('?', '').split('&'),
+                atributoValor = '',
+                atributoDaUrl = window.location.search.replace('?', '').split('=')[0];
+
+            params.map(function(index, element) {
+                paramsUrl.push(index.split('='));
+            });
+
+            paramsUrl.map(function(element, index) {
+                if (element[0] == 'categoria') {
+                    atributoValor = element;
+                }
+            });
+
+            if (atributoValor[0] == 'categoria') {
+                window.location.href = window.location.origin +
+                    window.location.pathname +
+                    '?' + atributoValor[0] + '=' + atributoValor[1] +
+                    '&atributo=' + filtro.val();
+            } else {
+                window.location.href = window.location.origin +
+                    window.location.pathname + '?atributo=' + filtro.val();
+            }
+        } else {
+            window.location.href = window.location.origin +
+                window.location.pathname + '?atributo=' + filtro.val();
+        }
+    });
 </script>
 
 <?php get_footer(); ?>
